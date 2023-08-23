dafe8b95c22b8577a877609f059327eb1bf120af
Seth Merck <113151310+sethmerck@users.noreply.github.com>
2023-08-23 17:25:31 -0400

    Create streamlit_figure.py

---
+++ streamlit_figure.py
@@ -0,0 +1,20 @@
+### creates chart with the most recent csv file in folder
+
+import os
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+recent_file = [f for f in os.listdir('test_folder')][-1]
+
+data = pd.read_csv(f'test_folder\{recent_file}') #path folder of the data file
+data = data.drop(data[data["Mileage"]<5].index)
+
+plot = sns.regplot(x=data['Mileage'],y=data['Price'], data=data, line_kws={"color": "red"}, fit_reg=True, logx=True, truncate=True)
+plt.xlabel('Mileage', fontsize = 22, labelpad=21)
+plt.ylabel('Price', fontsize = 22, labelpad=21)
+plt.ylim(0, 250000)
+plt.xlim(0, 350000)
+
+st.pyplot(plot.get_figure())
