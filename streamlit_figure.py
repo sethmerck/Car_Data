5e7179d24ae7c1deee05825ecd2fa01272d6761c
Seth Merck <113151310+sethmerck@users.noreply.github.com>
2023-08-27 13:13:20 -0400

    Update streamlit_figure.py

--- streamlit_figure.py
+++ streamlit_figure.py
@@ -15,9 +15,13 @@ start_t = lines[0]
 end_t = lines[-1]
 w, z = st.select_slider('Date Collected',options=lines, value=[start_t,end_t])
 
+prev_file = f"test_actions{lines.index(w)+1}.csv"
 recent_file = f"test_actions{lines.index(z) + 1}.csv"
 
+prev_data = pd.read_csv(f'working_dataset/{prev_file}')
 data = pd.read_csv(f'working_dataset/{recent_file}') #path folder of the data file
+
+prev_data = prev_data.drop(prev_data[prev_data["Mileage"]<5].index)
 data = data.drop(data[data["Mileage"]<5].index)
 
 plot = sns.regplot(x=data['Mileage'],y=data['Price'], data=data, line_kws={"color": "red"}, fit_reg=True, logx=True, truncate=True)
@@ -34,10 +38,17 @@ brands = []
 for i in f.readlines():
     brands.append(i.strip())
 
+prev_data['Car'] = prev_data['Car'].str.split(' ')
 data['Car'] = data['Car'].str.split(' ')
+
+prev_data = prev_data.explode('Car')
 data = data.explode('Car')
+
+prev_data = prev_data.query(f"Car in {brands}")
 data = data.query(f"Car in {brands}")
 
-
+prev_data_grouped = prev_data.groupby(by="Car")["Price"].agg([np.mean, np.std, 'min', 'max', 'count'])
 data_grouped = data.groupby(by="Car")["Price"].agg([np.mean, np.std, 'min', 'max', 'count'])
+
+data_grouped['diff'] = data_grouped['count'] - prev_data_grouped['count']
 st.dataframe(data_grouped,use_container_width=True)
