a0b11220f42042e05d9c1377b2768f83cf5162cc
Seth Merck <113151310+sethmerck@users.noreply.github.com>
2023-08-26 21:42:16 -0400

    Update streamlit_figure.py

--- streamlit_figure.py
+++ streamlit_figure.py
@@ -9,14 +9,7 @@ with open('status.log', 'r') as logs:
     lines = logs.readlines()[-6:]
     lines = [i[:10] for i in lines]
 
-# r = st.slider('csv_value', min_value=1, max_value=len(os.listdir('working_dataset')))
-
 z = st.select_slider('Select a range of color wavelength',options=lines)
-
-# def index_containing_substring(lines, z):
-#     for i, s in enumerate(lines):
-#         if z in s:
-#               return s
 
 recent_file = f"test_actions{lines.index(z) + 1}.csv"
 
