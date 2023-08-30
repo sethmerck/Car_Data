cba7428e8f046fa6d021169eeffb86d4deaceb8d
Seth Merck <113151310+sethmerck@users.noreply.github.com>
2023-08-30 14:47:05 -0400

    Update streamlit_figure.py

--- streamlit_figure.py
+++ streamlit_figure.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 from datetime import datetime
+from PIL import Image
 
 # st.set_page_config(layout="wide")
 # with open('status.log', 'r') as logs:
