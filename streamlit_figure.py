4d4a10362165237fa94dcd6e26bcd3cb9daa7f54
Seth Merck <113151310+sethmerck@users.noreply.github.com>
2023-09-03 01:27:08 -0400

    Update streamlit_figure.py

--- streamlit_figure.py
+++ streamlit_figure.py
@@ -75,7 +75,7 @@ box = sorted_data.boxplot(column='Price', by="Car", rot=45, ax=v)
 
 labels = box.get_xticklabels(which='major')
 counts = sorted_data.groupby(by="Car")["Price"].count().tolist()
-box.set_xticks(ticks=[1,2,3,4,5], labels=[v for i,v in enumerate(labels)])
+box.set_xticks(ticks=[1,2,3,4,5], labels=[f"{v}" for i,v in enumerate(labels)])
 st.pyplot(a)
 
 prev_data_grouped = prev_data.groupby(by="Car")["Price"].agg([np.mean, np.std, 'min', 'max', 'count'])
