69bdcf812333d208952502d9dfb527c606270828
Seth Merck <113151310+sethmerck@users.noreply.github.com>
2023-11-08 16:15:36 -0500

    Update streamlit_figure.py

--- streamlit_figure.py
+++ streamlit_figure.py
@@ -181,7 +181,7 @@ st.write("")
 st.caption("""I found American made cars (Chevrolet and Ford) had more listings and their price distributions skewed higher compared to the other three most common car 
            makes (Honda, Nissan, Toyota). However, I did not find much difference in the mileage distributions for listings of these five brands. The next few weeks will be spent 
            discovering any new insights to be found in my datasets as more data comes in. As time progresses and my dataset grows, I'd like the ability to observe any 
-           broader trends (such as noticeable changes in price) that may be occurring within my local used car market.\n\nUpdate as of 11/8/2023: The median mileage of listings in the top 5 makes has been decreasing recently. Price distributions have remained relatively 
+           broader trends (such as noticeable changes in price) that may be occurring within my local used car market.\n\nAs of 11/8/2023: The median mileage of listings in the top 5 makes has been decreasing recently. Price distributions have remained relatively 
            consistent. Overall that means a better value for those purchasing a used vehicle. This suggests to me that it's possible people are trying to sell their 
            secondary or tertiary vehicles for some quick cash. Makes sense with a recession looming.""")
 
