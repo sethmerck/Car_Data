89eaeae3d47866626f16f41d10e5a8bb46e989fd
Seth Merck <113151310+sethmerck@users.noreply.github.com>
2023-11-19 11:51:44 -0500

    Update streamlit_figure.py

--- streamlit_figure.py
+++ streamlit_figure.py
@@ -157,10 +157,9 @@ for i, v in enumerate(lines):
 fig = plt.figure(figsize=(8,8))
 for brand in master_brand_dict:
     # datetime.datetime.combine(i, datetime.time.min)  for i in lines
-    x_vals = [item[0] for item in master_brand_dict[brand]]
+    x_vals = [datetime.datetime.combine(item[0], datetime.time.min).timestamp() for item in master_brand_dict[brand]]
     y_vals = [item[1] for item in master_brand_dict[brand]]
     plt.scatter(x_vals, y_vals, label=brand)
-    x_vals = [datetime.datetime.combine(item[0], datetime.time.min).timestamp() for item in master_brand_dict[brand]]
     plt.plot(x_vals, np.poly1d(np.polyfit(x_vals, y_vals, 2))(x_vals))
     plt.legend()
     plt.xlabel('Date')
