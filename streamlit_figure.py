4ac66a784ebe78d91c1dde18d97e7a0187e68e70
Seth Merck <113151310+sethmerck@users.noreply.github.com>
2023-11-19 16:56:44 -0500

    Update streamlit_figure.py

--- streamlit_figure.py
+++ streamlit_figure.py
@@ -276,8 +276,8 @@ for brand in master_brand_dict:
         
         y_vals = [item[1]["Mileage"] for item in master_brand_dict[brand]]
         
-        m, b, r_value, p_value, std_err = scipy.stats.linregress(x_vals, y_vals)
-        plt.annotate(f'R^2 = {r_value**2}')
+        # m, b, r_value, p_value, std_err = scipy.stats.linregress(x_vals, y_vals)
+        # plt.annotate(f'R^2 = {r_value**2}', xy=())
      
         
         
