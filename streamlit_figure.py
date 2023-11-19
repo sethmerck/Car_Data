6c204a01f8269c73e5c33cc91bf093b19189e4f3
Seth Merck <113151310+sethmerck@users.noreply.github.com>
2023-11-19 12:45:54 -0500

    Update streamlit_figure.py

--- streamlit_figure.py
+++ streamlit_figure.py
@@ -137,7 +137,7 @@ for i, v in enumerate(lines):
     df = df.groupby(by="Car", as_index=False)["Mileage"].agg([np.median, 'count'])
     df = df.drop(df[df["count"]<300].index)
     df = df.rename(columns={'median': 'Mileage'})
-    brand_dict = pd.Series(df.Mileage.values, index=df.Car).to_dict()
+    brand_dict = pd.Series(df['count'].values, index=df.Car).to_dict()
     
     st.write(brand_dict)
     
