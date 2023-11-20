7c6a40c8b0de95e0813669608b01213d90038fa7
Seth Merck <113151310+sethmerck@users.noreply.github.com>
2023-11-19 19:34:48 -0500

    Update streamlit_figure.py

--- streamlit_figure.py
+++ streamlit_figure.py
@@ -281,7 +281,7 @@ for brand in master_brand_dict:
      
         
         
-        plt.scatter(x_vals, y_vals, label=f"{brand}:  R^2 = {round(r_value**2,2)}")
+        plt.scatter(x_vals, y_vals, label=f"{brand}:  R^2 = {round(r_value**2,2)}", s=50)
         plt.plot(x_vals, np.poly1d(np.polyfit(x_vals, y_vals, 1))(x_vals))
         plt.legend(fontsize=8)
         plt.xticks(ticks=[x_vals[0], x_vals[int(len(x_vals)/2) + 1], x_vals[-1]], labels=[x_tick_vals[0], x_tick_vals[int(len(x_vals)/2) + 1], x_tick_vals[-1]])
