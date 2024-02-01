cca8538fe1bf1b8825878d20866981f4b710a6e9
Seth Merck <113151310+sethmerck@users.noreply.github.com>
2024-01-31 19:33:31 -0500

    Update streamlit_figure.py

--- streamlit_figure.py
+++ streamlit_figure.py
@@ -300,15 +300,15 @@ st.dataframe(data_grouped,use_container_width=True)
 
 st.write("")
 
-# master_brand_dict = {}
-# for i, v in enumerate(lines[lines.index(w):lines.index(z)+1]):
-#     file = f"test_actions{i+1}.csv"
-#     df = pd.read_csv(f'working_dataset/{file}')
-#     df = df.drop(df[df["Mileage"]<100].index)
-#     df = df.drop(df[df["Price"]<100].index)
-#     df = df[df["Car"].str.contains('Accord')]
-# #     st.write(df)
-# #     # df['Car'] = df['Car'].str.split(' ')
+master_brand_dict = {}
+for i, v in enumerate(lines[lines.index(w):lines.index(z)+1]):
+    file = f"test_actions{i+1}.csv"
+    df = pd.read_csv(f'working_dataset/{file}')
+    df = df.drop(df[df["Mileage"]<100].index)
+    df = df.drop(df[df["Price"]<100].index)
+    df = df[df["Car"].str.contains('Accord')]
+#     st.write(df)
+#     # df['Car'] = df['Car'].str.split(' ')
 
 #     # df = df.explode('Car')
     
@@ -331,11 +331,11 @@ st.write("")
 #     # # st.write(df)
 #     # # df = df[['Mileage', 'count', 'Car']]
     
-#     df.set_index('Car',inplace=True)
-#     brand_dict = df.to_dict()
-#     # st.write(brand_dict["Price"])
-#     master_brand_dict[v] = brand_dict["Price"]
-# st.write(master_brand_dict)
+    df.set_index('Car',inplace=True)
+    brand_dict = df.to_dict()
+    # st.write(brand_dict["Price"])
+    master_brand_dict[i] = (v, brand_dict["Price"])
+st.write(master_brand_dict)
     
 # for key in brand_dict:
 #     if key in master_brand_dict:
