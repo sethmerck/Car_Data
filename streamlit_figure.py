95f4400644923d49f6d5f0450d5d2ddddf91c4b0
Seth Merck <113151310+sethmerck@users.noreply.github.com>
2024-02-06 00:50:06 -0500

    Update streamlit_figure.py

--- streamlit_figure.py
+++ streamlit_figure.py
@@ -383,7 +383,7 @@ for brand in master_brand_dict:
     # x_vals[int(len(x_vals)/2) + 1] x_tick_vals[int(len(x_vals)/2) + 1], 
     plt.xlabel('Date', labelpad=15)
     plt.ylabel('Median Price', labelpad=15)
-    plt.title("Change in Median Price of Most Common Makes Over Time", pad=10)
+    plt.title("Work in Progress, will show data just related to my focus, Honda Vehicles", pad=10)
 st.pyplot(fig)
 
 
