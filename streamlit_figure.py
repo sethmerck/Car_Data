87ad120523a8421159f060af660dc5267ed69e1f
Seth Merck <113151310+sethmerck@users.noreply.github.com>
2024-02-06 11:13:33 -0500

    Update streamlit_figure.py

--- streamlit_figure.py
+++ streamlit_figure.py
@@ -313,7 +313,7 @@ for i, v in enumerate(lines[lines.index(w):lines.index(z)+1]):
     brand_dict = df.to_dict()
 # st.write(brand_dict["Price"])
     master_brand_dict[i] = (v, brand_dict["Price"])
-zed = [list(master_brand_dict[item][1].keys()) for item in master_brand_dict]
+zed = sorted([list(master_brand_dict[item][1].keys()) for item in master_brand_dict])
 
 st.write(zed)
 #     st.write(df)
