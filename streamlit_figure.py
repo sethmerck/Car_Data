d5957f3a32f9b019dc45bf06f3cc4abf08043f48
Seth Merck <113151310+sethmerck@users.noreply.github.com>
2024-02-13 14:23:37 -0500

    Update streamlit_figure.py

--- streamlit_figure.py
+++ streamlit_figure.py
@@ -328,7 +328,7 @@ for i, v in enumerate(lines[lines.index(w):lines.index(z)+1]):
             master_brand_dict[key] = list_of_two
 #st.write(master_brand_dict)
 fig = plt.figure(figsize=(8,8))
-p=1
+
 for brand in master_brand_dict:
     counts = [item[1]["Count"] for item in master_brand_dict[brand]]
 
@@ -349,13 +349,12 @@ for brand in master_brand_dict:
  
     plt.scatter(x_vals, y_vals, label=f"{brand}", s=10)
     plt.plot(x_vals, np.poly1d(np.polyfit(x_vals, y_vals, 1))(x_vals))
-    plt.legend(labels = [p], fontsize=8, loc='upper right')
-    p+=1
+
     # x_vals[int(len(x_vals)/2) + 1] x_tick_vals[int(len(x_vals)/2) + 1], 
     plt.xlabel('Date', labelpad=15)
     plt.ylabel('Median Price', labelpad=15)
     plt.title("Change in Median Price of Honda Accord Generations Over Time", pad=10)
-
+plt.legend(labels = [brand for brand in master_brand_dict], fontsize=8, loc='upper right')
 st.pyplot(fig)
 #     #df.set_index('Car',inplace=True)
 #     # brand_dict = df.to_dict('index')
