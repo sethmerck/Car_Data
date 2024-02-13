b44599075c8318bbd4d227db29ad22af2d94d060
Seth Merck <113151310+sethmerck@users.noreply.github.com>
2024-02-13 11:48:34 -0500

    Update streamlit_figure.py

--- streamlit_figure.py
+++ streamlit_figure.py
@@ -352,7 +352,7 @@ for brand in master_brand_dict:
     plt.ylabel('Median Price', labelpad=15)
     plt.title("Change in Median Price of Honda Accord Generations Over Time", pad=10)
     
-# plt.xticks(ticks=[w, z], labels=[x_tick_vals[0], x_tick_vals[-1]])
+plt.xticks(ticks=lines[lines.index(w), lines.index(z)+1], labels=lines[lines.index(w), lines.index(z)+1])
 st.pyplot(fig)
 #     #df.set_index('Car',inplace=True)
 #     # brand_dict = df.to_dict('index')
