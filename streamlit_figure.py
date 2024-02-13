9fae9ac3e6b26ab1df297b72799ae11782dcc4fe
Seth Merck <113151310+sethmerck@users.noreply.github.com>
2024-02-13 12:31:28 -0500

    Update streamlit_figure.py

--- streamlit_figure.py
+++ streamlit_figure.py
@@ -355,7 +355,7 @@ for brand in master_brand_dict:
     plt.title("Change in Median Price of Honda Accord Generations Over Time", pad=10)
 
 
-plt.xticks(ticks=[x_vals[0], x_vals[-1]], labels=[x_tick_vals[0], x_tick_vals[-1]])
+# plt.xticks(ticks=[x_vals[0], x_vals[-1]], labels=[x_tick_vals[0], x_tick_vals[-1]])
 #plt.xticks(ticks=[lines.index(w), lines.index(z)+1], labels=[lines.index(w), lines.index(z)+1])
 
 st.pyplot(fig)
