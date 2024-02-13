ea9a0ec0f85ec2abbd65e26676e15cfaac6f19c8
Seth Merck <113151310+sethmerck@users.noreply.github.com>
2024-02-13 14:37:00 -0500

    Update streamlit_figure.py

--- streamlit_figure.py
+++ streamlit_figure.py
@@ -350,7 +350,7 @@ for brand in master_brand_dict:
  
     plt.scatter(x_vals, y_vals, label=f"{brand}", s=10)
     plt.plot(x_vals, np.poly1d(np.polyfit(x_vals, y_vals, 1))(x_vals))
-
+    plt.legend(fontsize=8, loc='upper right')
     # x_vals[int(len(x_vals)/2) + 1] x_tick_vals[int(len(x_vals)/2) + 1], 
     plt.xlabel('Date', labelpad=15)
     plt.ylabel('Median Price', labelpad=15)
